c226f708acda012d807a1e9475d109aaaa361a39
Deblyn Prado <deblynprado@hotmail.com>
2025-02-02 16:37:34 -0300

    Showing 'Say Hello' message

--- UIKit/Welcome/Welcome/ViewController.swift
+++ UIKit/Welcome/Welcome/ViewController.swift
@@ -11,10 +11,12 @@ class ViewController: UIViewController {
     
     @IBOutlet var inputName: UITextField!
     
-    
     @IBAction func buttonHello(_ sender: Any) {
+        let helloText: String = "Say Hello,"
+//        Perguntar ao professor por que "World" não está aparecendo quando nome está vazio.
+        textAreaHello.text = "\(helloText) \(inputName.text ?? "World")!"
     }
-    
+
     
     @IBOutlet var textAreaHello: UITextView!
 }
