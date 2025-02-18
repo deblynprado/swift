2081ee7a562a0faa1ceedbab69d00533b866ea28
Deblyn Prado <deblynprado@hotmail.com>
2025-02-18 20:19:39 -0300

    Improving average app

--- UIKit/Average/Average/ViewController.swift
+++ UIKit/Average/Average/ViewController.swift
@@ -13,22 +13,42 @@ class ViewController: UIViewController {
     @IBOutlet var resultLabel: UILabel!
     @IBOutlet var inputNameOne: UITextField!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        inputNametwo.keyboardType = .decimalPad
+        inputNameOne.keyboardType = .decimalPad
+    }
+    
     @IBAction func calcButton(_ sender: Any) {
         
-        guard let scoreOne = Double(inputNameOne.text ?? "0"),
-              let scoreTwo = Double(inputNametwo.text ?? "0") else {
+        guard let scoreOne = Double(inputNameOne.text ?? ""),
+              let scoreTwo = Double(inputNametwo.text ?? "") else {
             resultLabel.text = "Digite apenas números"
             return
         }
         
         let average = (scoreOne + scoreTwo) / 2
         
-        if average >= 0 && average <= 5 {
-            resultLabel.text = "Média \(average) - Reprovado"
-        } else if average > 5 && average < 7 {
-            resultLabel.text = "Média \(average) - Em Recuperação"
-        } else {
-            resultLabel.text = "Média \(average) - Aprovado"
+        var type: String = "Ocorreu um erro"
+        
+        if average >= 0 && average <= 4 {
+            type = "Reprovado"
+        } else if average > 4 && average < 7 {
+            type = "Em recuperação"
+        } else if average >= 7 && average <= 10 {
+            type = "Aprovado"
         }
+        
+        resultLabel.text = """
+                           Sua média é \(average)
+                           Status: \(type)
+                           """
     }
 }
+
+
+
+//Which of these lines is valid if we may or may not have an integer value for httpResponse? (Choose 3.)
+//Review the code sample to answer the question. What code goes in the blank? temperatureCheck()
+//Review the code sample to answer the question. What code goes in the blank? peopleAdmit()
+var http = 0
