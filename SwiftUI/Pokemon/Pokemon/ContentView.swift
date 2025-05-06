57c41b83d5fb079dbad6bab267856771e2943c84
Deblyn Prado <deblynprado@hotmail.com>
2025-05-05 23:31:12 -0300

    Adding image size

--- SwiftUI/Pokemon/Pokemon/ContentView.swift
+++ SwiftUI/Pokemon/Pokemon/ContentView.swift
@@ -11,12 +11,24 @@ struct ContentView: View {
     @State var pokemons: [Pokemon] = []
     var body: some View {
         List(pokemons) { pokemon in
-            Text(pokemon.name)
-            AsyncImage(url: pokemon.cover.image)
+            HStack {
+                Text(pokemon.name)
+                AsyncImage(
+                    url: pokemon.cover.image,
+                    content: { image in
+                        image.resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxHeight: 100)
+                    },
+                    placeholder: {
+                        ProgressView()
+                    }
+                )
+            }
         }
         .onAppear() {
             guard pokemons.isEmpty else { return }
-            let cover: PokemonCover = .init(indexImage: 1)
+            let cover: PokemonCover = .init(indexImage: 4)
             let pokemon = Pokemon(name: "Pikachu", cover: cover)
             pokemons.append(pokemon)
         }
